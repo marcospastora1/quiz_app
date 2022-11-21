fba790d4b27532e33595ae4e84d7cd1f62d50e85
Marcos <marcospastora@gmail.com>
2022-11-20 23:46:25 -0300

    adicionando imagens para o menu

---
+++ lib/domain/core/constants/assets.constants.dart
@@ -0,0 +1,10 @@
+abstract class AssetsConstants {
+  static const _pathImages = 'assets/images';
+
+  static const String ciencias = '$_pathImages/ciencias.jpg';
+  static const String historia = '$_pathImages/historia.jpg';
+  static const String geografia = '$_pathImages/geografia.jpg';
+  static const String portugues = '$_pathImages/portugues.jpg';
+  static const String matematica = '$_pathImages/matematica.jpg';
+  static const String gerais = '$_pathImages/gerais.jpg';
+}
