efde0d5db5ea34e7c578c163b51f505440813017
Marcos <marcospastora@gmail.com>
2022-11-21 11:11:01 -0300

    criando func jogar novamente e refatorando para pegar somente 5 questões aleatorias da api, não importando a quantidade que venha

--- lib/presentation/quiz/controllers/quiz.controller.dart
+++ lib/presentation/quiz/controllers/quiz.controller.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 import 'package:quiz_app/domain/questions/models/question.model.dart';
 import 'package:quiz_app/domain/questions/questions.repository.dart';
@@ -18,18 +19,22 @@ class QuizController extends GetxController {
   }
 
   final questions = <QuestionModel>[].obs;
+  final questionsEscolhidas = <QuestionModel>[].obs;
+  final scoreKeeper = <Widget>[].obs;
+  final totalEscolhidas = 5;
   final random = Random();
   final hitNumber = 0.obs;
   final questionIndex = 0.obs;
   late final bool shiftAnswer;
 
-  int get questionsNumber => questions.length;
-  QuestionModel get question => questions[questionIndex.value];
+  int get questionsNumber => questionsEscolhidas.length;
+  QuestionModel get question => questionsEscolhidas[questionIndex.value];
 
   @override
   onReady() {
     super.onReady();
     loadQuestions();
+    shiftAnswer = random.nextBool();
   }
 
   Future<void> loadQuestions() async {
@@ -37,9 +42,14 @@ class QuizController extends GetxController {
       _loading.isLoading = true;
       final response = await _questionsRepository.getQuestions(quiz: quiz);
       questions.assignAll(response);
-      questions.shuffle();
-      questions[questionIndex.value].answers.shuffle();
-      shiftAnswer = random.nextBool();
+      // questions.shuffle();
+      // questions[questionIndex.value].answers.shuffle();
+      for (var i = 0; i < totalEscolhidas; i++) {
+        questions.shuffle();
+        questionsEscolhidas.add(questions.last);
+        questions.remove(questions.last);
+        questionsEscolhidas[questionIndex.value].answers.shuffle();
+      }
     } catch (err) {
       SnackbarUtil.showWarning(
           message: 'Não foi possível pegar as perguntas 😢');
@@ -50,66 +60,82 @@ class QuizController extends GetxController {
   }
 
   void nextQuestion() {
-    questionIndex.value = ++questionIndex.value % questions.length;
+    questionIndex.value = ++questionIndex.value % questionsEscolhidas.length;
     question.answers.shuffle();
   }
 
   String getQuestion() {
-    return questions[questionIndex.value].question;
+    return questionsEscolhidas[questionIndex.value].question;
   }
 
   String getAnswer1() {
     return shiftAnswer
-        ? questions[questionIndex.value].answers[0].resposta
+        ? questionsEscolhidas[questionIndex.value].answers[0].resposta
         : shiftAnswer
-            ? questions[questionIndex.value].answers[1].resposta
+            ? questionsEscolhidas[questionIndex.value].answers[1].resposta
             : shiftAnswer
-                ? questions[questionIndex.value].answers[2].resposta
-                : questions[questionIndex.value].answers[3].resposta;
+                ? questionsEscolhidas[questionIndex.value].answers[2].resposta
+                : questionsEscolhidas[questionIndex.value].answers[3].resposta;
   }
 
   String getAnswer2() {
     return shiftAnswer
-        ? questions[questionIndex.value].answers[1].resposta
+        ? questionsEscolhidas[questionIndex.value].answers[1].resposta
         : shiftAnswer
-            ? questions[questionIndex.value].answers[2].resposta
+            ? questionsEscolhidas[questionIndex.value].answers[2].resposta
             : shiftAnswer
-                ? questions[questionIndex.value].answers[3].resposta
-                : questions[questionIndex.value].answers[0].resposta;
+                ? questionsEscolhidas[questionIndex.value].answers[3].resposta
+                : questionsEscolhidas[questionIndex.value].answers[0].resposta;
   }
 
   String getAnswer3() {
     return shiftAnswer
-        ? questions[questionIndex.value].answers[2].resposta
+        ? questionsEscolhidas[questionIndex.value].answers[2].resposta
         : shiftAnswer
-            ? questions[questionIndex.value].answers[3].resposta
+            ? questionsEscolhidas[questionIndex.value].answers[3].resposta
             : shiftAnswer
-                ? questions[questionIndex.value].answers[0].resposta
-                : questions[questionIndex.value].answers[1].resposta;
+                ? questionsEscolhidas[questionIndex.value].answers[0].resposta
+                : questionsEscolhidas[questionIndex.value].answers[1].resposta;
   }
 
   String getAnswer4() {
     return shiftAnswer
-        ? questions[questionIndex.value].answers[3].resposta
+        ? questionsEscolhidas[questionIndex.value].answers[3].resposta
         : shiftAnswer
-            ? questions[questionIndex.value].answers[0].resposta
+            ? questionsEscolhidas[questionIndex.value].answers[0].resposta
             : shiftAnswer
-                ? questions[questionIndex.value].answers[1].resposta
-                : questions[questionIndex.value].answers[2].resposta;
+                ? questionsEscolhidas[questionIndex.value].answers[1].resposta
+                : questionsEscolhidas[questionIndex.value].answers[2].resposta;
   }
 
   bool correctAnswer(String answer) {
-    var correct = questions[questionIndex.value].answers[0].verdadeira
-        ? questions[questionIndex.value].answers[0].resposta == answer
-        : questions[questionIndex.value].answers[1].verdadeira
-            ? questions[questionIndex.value].answers[1].resposta == answer
-            : questions[questionIndex.value].answers[2].verdadeira
-                ? questions[questionIndex.value].answers[2].resposta == answer
-                : questions[questionIndex.value].answers[3].verdadeira
-                    ? questions[questionIndex.value].answers[3].resposta ==
+    var correct = questionsEscolhidas[questionIndex.value].answers[0].verdadeira
+        ? questionsEscolhidas[questionIndex.value].answers[0].resposta == answer
+        : questionsEscolhidas[questionIndex.value].answers[1].verdadeira
+            ? questionsEscolhidas[questionIndex.value].answers[1].resposta ==
+                answer
+            : questionsEscolhidas[questionIndex.value].answers[2].verdadeira
+                ? questionsEscolhidas[questionIndex.value]
+                        .answers[2]
+                        .resposta ==
+                    answer
+                : questionsEscolhidas[questionIndex.value].answers[3].verdadeira
+                    ? questionsEscolhidas[questionIndex.value]
+                            .answers[3]
+                            .resposta ==
                         answer
                     : false;
     hitNumber.value = hitNumber.value + (correct ? 1 : 0);
     return correct;
+  }
+
+  void jogarNovamente() {
+    questionIndex.value = 0;
+    hitNumber.value = 0;
+    questions.value = [];
+    scoreKeeper.value = [];
+    questionsEscolhidas.value = [];
+    loadQuestions();
+    Get.back();
   }
 }
